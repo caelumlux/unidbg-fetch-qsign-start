02e7ca5366a443c276b8e6df000bd6f8454493f3
TeaBoss <2220067959@qq.com>
2023-10-28 20:54:09 +0800

    修复企点版本号问题

--- startup.bat
+++ startup.bat
@@ -1,4 +1,4 @@
 @echo off
-set /p var=请输入要开启的版本(支持:8.9.50~8.9.88,Tim3.5.1/2/5,企点3.8.2)
+set /p var=璇疯緭鍏ヨ寮�鍚殑鐗堟湰(鏀寔:8.9.50~8.9.88,Tim3.5.1/2/5,浼佺偣5.8.2)
 bin\unidbg-fetch-qsign.bat --basePath=txlib\%var%
 pause
