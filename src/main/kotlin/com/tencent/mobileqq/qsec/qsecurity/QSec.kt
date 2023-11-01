48aac63ec855a8f1d9436c1be7e5a52487cf2926
TeaBoss <2220067959@qq.com>
2023-11-01 18:20:38 +0800

    Update QSec.kt
    add GeXwdbgID

--- src/main/kotlin/com/tencent/mobileqq/qsec/qsecurity/QSec.kt
+++ src/main/kotlin/com/tencent/mobileqq/qsec/qsecurity/QSec.kt
@@ -3,7 +3,6 @@ package com.tencent.mobileqq.qsec.qsecurity
 import com.github.unidbg.linux.android.dvm.BaseVM
 import com.github.unidbg.linux.android.dvm.DvmObject
 import com.github.unidbg.linux.android.dvm.array.ByteArray
-import com.tencent.mobileqq.fe.FEKit
 import moe.fuqiuluo.unidbg.QSecVM
 
 object QSec {
@@ -11,9 +10,7 @@ object QSec {
         vm.newInstance("com/tencent/mobileqq/qsec/qsecurity/QSec", unique = true)
             .callJniMethodInt(vm.emulator, "doSomething(Landroid/content/Context;I)I", context, 1)
     }
-    fun getXwdbugID(vm: QSecVM, qq:String):kotlin.ByteArray{
-        return FEKit.xwDebugID
-    }
+
     fun getEst(vm: QSecVM): ByteArray? {
         val context = (vm.vm as BaseVM).resolveClass("android/content/Context", vm.vm.resolveClass("java/io/File")).newObject(null)
         return vm.newInstance("com/tencent/mobileqq/qsec/qsecest/QsecEst", unique = true)
@@ -21,4 +18,12 @@ object QSec {
                 vm.emulator, "d(Landroid/content/Context;Ljava/lang/String;Ljava/lang/String;)[B",
                 context, vm.envData.guid, "")
     }
+    fun getXwdbugID(vm: QSecVM,QQ:String): kotlin.ByteArray {
+        val context = (vm.vm as BaseVM).resolveClass("android/content/Context", vm.vm.resolveClass("java/io/File")).newObject(null)
+        return vm.newInstance("com/tencent/mobileqq/qsec/qsecurity/QSec", unique = true)
+            .callJniMethodObject<DvmObject<*>>(
+                vm.emulator, "getXwDebugID(Ljava/lang/String;)[B",
+                QQ).value as kotlin.ByteArray
+    }
+
 }
